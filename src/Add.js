afa2d5a2783e5632955a2a7d9f00da6d4cafad6a
noah-aoki-mt <noa.aoki@metateam.co.jp>
2024-06-17 13:05:23 +0900

    貸出画面編集後

--- src/Add.js
+++ src/Add.js
@@ -1,20 +1,87 @@
 import React from 'react'
+import { useState } from 'react'
 
 const Add = () => {
+    const [employeeName, setEmployeeName] = useState('');
+    const [num, setNum] = useState(0);
+    const currentDate = new Date();
+    const [rentalAt, setRentalAt] = useState('');
+    const [returnAt, setReturnAt] = useState('');
+    const [status, setStatus] = useState('');
+
+
+    const handleNamechange = (event) => {
+        setEmployeeName(event.target.value);
+    }
+
+    const handleCountUp = () => {
+        setNum(prevState => prevState + 1)
+    }
+    const handleCountDown = () => {
+        if (num > 0) {
+            setNum(prevState => prevState - 1)
+        } else {
+            alert('0以下は入力できません')
+        }
+    }
+
+    const handleRentalAt = (event) => {
+        const selectedDate = new Date(event.target.value);
+        if (selectedDate >= currentDate) {
+            setRentalAt(event.target.value)
+        } else {
+            alert('過去の日付けは入力できません')
+        }
+    }
+
+    const handleReturnAt = (event) => {
+        const selectedDate = new Date(event.target.value);
+        if (selectedDate >= new Date(rentalAt)) {
+            setReturnAt(event.target.value)
+        } else {
+            alert('貸出日より過去の日付けは入力できません')
+        }
+    }
+
+    const handleStatusChange = (event) => {
+        setStatus(event.target.value)
+    }
+
+    const handleAdd = () =>{
+        const newData = {
+            employeeName : employeeName,
+            num : num,
+            rentalAt : rentalAt,
+            returnAt : returnAt,
+            status : status
+        };
+
+        console.log("新規データ：",newData)
+
+        setEmployeeName('');
+        setNum(0);
+        setRentalAt('');
+        setReturnAt('');
+        setStatus('');
+    }
     return (
         <div>
-            <p>社員番号</p>
-            <input type="text" />
-            <p>貸出書籍</p> 
-            <input typpe="text" />
-            <p>貸出日</p>
-            <input type="date" />
-            <p>返却日</p>
-            <input type="date" />
-            <p>貸出ステータス</p>
-            <input type="text" />
+            <p>社員名：{employeeName}</p>
+            <input type="text" onChange={handleNamechange} />
+            <p>貸出書籍数：{num}</p>
+            <button onClick={handleCountUp}>+</button>
+            <button onClick={handleCountDown}>-</button>
+            <p>貸出日：{rentalAt}</p>
+            <input type="date" onChange={handleRentalAt} />
+            <p>返却日：{returnAt}</p>
+            <input type="date" onChange={handleReturnAt} />
+            <p>貸出ステータス：{status}</p>
+            <input type="text" onChange={handleStatusChange} />
+            <p>
+                <button onClick={handleAdd}>保存</button>
+            </p>
         </div>
     )
 }
 
-export default Add
+export default Add;
--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ import { useState } from 'react';
 
 function App() {
   const lists = ["書籍一覧", "在庫一覧", "在庫カレンダー", "貸出一覧"];
-  const handleAdd = () => { }
   return (  
     <>
       <header className="App-header">
@@ -17,9 +16,6 @@ function App() {
       </div>
       <div className='App-body'>
         <Add />
-        <p>
-        <button onClick={handleAdd}>保存</button>
-        </p>
       </div>
     </>
   );
